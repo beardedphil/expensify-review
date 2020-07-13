9efe6c0e85a1292ff97ab72e7cc46237430f085e
Phil King <phil@beardedphil.com>
2020-07-13 07:05:40 -0500

    Removed unused imports.

--- src/app.js
+++ src/app.js
@@ -6,9 +6,6 @@ import configureStore from './store/configureStore';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-import getVisibleExpenses from './selectors/expenses';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
 
 const store = configureStore();
 
